10662237a1ac07afd1fc79a7c4ea413c90f7c462
AzubuikeIbe0 <X00191169@mytudublin.ie>
2022-12-01 18:02:00 +0000

    o4

--- Source.cpp
+++ Source.cpp
@@ -18,25 +18,25 @@ int main()
 	L3 = L;
 
 	L3.displayList(cout);
-	//cout << L3;
+	cout << L3;
 
 
-	//L.insertFront(C);
-	//L.insertFront(C);
-	//
-	//cout << L;
+	L.insertFront(C);
+	L.insertFront(C);
+	
+	cout << L;
 
-	//L.insertBack(C.getName(), C.getNumber());
-	//L.insertBack(C.getName(), C.getNumber());
-	//L.insertBack(C.getName(), C.getNumber());
+	L.insertBack(C.getName(), C.getNumber());
+	L.insertBack(C.getName(), C.getNumber());
+	L.insertBack(C.getName(), C.getNumber());
 
-	//cout << L;
+	cout << L;
 
-	//L.deleteBack();
-	//L.deleteContact(C.getName());
-	//L.findContact(C.getName());
-	//L.isEmpty(); // return true if list empty
-	/*L.displayList(C);*/
+	L.deleteBack();
+	L.deleteContact(C.getName());
+	L.findContact(C.getName());
+	L.isEmpty(); // return true if list empty
+
 
 
 
